14a9af2cdfe864d6c0451b6f66373ebddebd97fb
beka00 <bekob550@gmail.com>
2022-03-30 21:18:17 +0600

    home work

---
+++ bin/lesson_12.dart
@@ -0,0 +1,99 @@
+void main(List<String> arguments) {
+  // List<int> myList = [1, 2, 3, 4, 5];
+  // myList.forEach((element) {
+  //   print(element * element);
+  // });
+
+  // List<int> myList = [1, 2, 3, 4, 5];
+  // var result = myList.map((e) {
+  //   return e * e;
+  // });
+  // print(result.toList());
+
+  // List<int> myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var result = myList.where((element) {
+  //   return element % 2 == 0;
+  // });
+  // print(result.toList());
+
+  // List myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // int result = myList.reduce((value, element) {
+  //   return value + element;
+  // });
+  // print(result);
+
+  // List myList = [3, 2, 1, 3, 6, 4];
+  // bool result = myList.every((element) {
+  //   return element < 5;
+  // });
+  // print(result);
+
+  // List myList = [-1, 0, 1, 2, 3, 4, 5];
+  // bool result = myList.any((element) {
+  //   return element < 0;
+  // });
+  // print(result);
+
+  //Домашняя работа
+
+//1)
+  // List<int> myList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var result = myList.forEach((element) {
+  //   print(element);
+  // });
+
+//2)
+  // List<int> myList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var result = myList.forEach((element) {
+  //   print(element * element * element);
+  // });
+
+  //3)
+  // List<int> myList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var result = myList.reduce((value, element) {
+  //   return value + element;
+  // });
+  // print(result);
+
+  //4)
+  // List<String> myList = ['any', 'boool', 'dance', 'city', 'every'];
+  // myList.sort();
+  // print(myList);
+
+  //5)
+  // List<String> myList = ['any', 'boool', 'dance', 'city', 'every'];
+  // myList.sort((a, b) {
+  //   return b.compareTo(a);
+  // });
+  // print(myList);
+
+  //6)
+  // List<String> myList = ['any', 'boool', 'dance', 'city', 'every'];
+  // myList.sort((a, b) {
+  //   return a.length.compareTo(b.length);
+  // });
+  // print(myList);
+
+  //7)
+  // List<int> myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // var result = myList.where((item) {
+  //   return item % 2 == 0;
+  // });
+  // print(result);
+
+//8)
+  // List<int> myList = [1, 4, 3, 7, 0, 9, 6];
+  // int res = 0;
+
+  // var result = myList.reduce((value, element) {
+  //   if (element < 1) {
+  //     res = value;
+  //     return value;
+
+  //   } else {
+  //     return value + element;
+  //   }
+  // });
+
+  // print(res);
+}
